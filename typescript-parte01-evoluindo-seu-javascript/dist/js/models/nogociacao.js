9504f7d567b21b9a5dd10699dfd9596314412e40
Alberto Lima <alberto.btlima@outlook.com>
2024-07-23 17:55:53 -0300

    add typescript

--- typescript-parte01-evoluindo-seu-javascript/dist/js/models/nogociacao.js
+++ typescript-parte01-evoluindo-seu-javascript/dist/js/models/nogociacao.js
@@ -1,11 +1,19 @@
 export class Negociacao {
-   #data;
-   #quantidade;
-   #valor;
-
-   constructor(data, quantidade, valor) {
-      this.#data = data;
-      this.#quantidade = quantidade;
-      this.#valor = valor;
-   }
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this.data;
+    }
+    get quantidade() {
+        return this.quantidade;
+    }
+    get valor() {
+        return this.valor;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
 }
