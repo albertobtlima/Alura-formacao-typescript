76b971967ff823d67fb5b27146da28fa340b9c91
Alberto Lima <alberto.btlima@outlook.com>
2024-08-01 12:09:22 -0300

    add decorator

--- typescript-parte03-mais-tecnicas-e-boas-praticas/app/src/views/view.ts
+++ typescript-parte03-mais-tecnicas-e-boas-praticas/app/src/views/view.ts
@@ -1,28 +1,33 @@
 export abstract class View<T> {
+   protected elemento: HTMLElement;
+   private escapar = false;
 
-    protected elemento: HTMLElement;
-    private escapar = false;
+   constructor(seletor: string, escapar?: boolean) {
+      const elemento = document.querySelector(seletor);
+      if (elemento) {
+         this.elemento = elemento as HTMLElement;
+      } else {
+         throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+      }
+      if (escapar) {
+         this.escapar = escapar;
+      }
+   }
 
-    constructor(seletor: string, escapar?: boolean) {
-        const elemento = document.querySelector(seletor);
-        if (elemento) {
-            this.elemento = elemento as HTMLElement;
-        } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
-        }
-    }
+   public update(model: T): void {
+      const t1 = performance.now();
 
-    public update(model: T): void {
-        let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
-    }
+      let template = this.template(model);
+      if (this.escapar) {
+         template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+      }
+      this.elemento.innerHTML = template;
 
-    protected abstract template(model: T): string;
+      const t2 = performance.now();
+      console.log(
+         `Tempo de execução do método update: ${(t2 - t1) / 1000} segundos`
+      );
+   }
+
+   protected abstract template(model: T): string;
 }
