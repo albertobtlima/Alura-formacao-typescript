c8263111badc23e9f6300d5aad4ce48e870c0e1d
Alberto Lima <alberto.btlima@outlook.com>
2024-08-01 18:33:48 -0300

    add dom-injector.ts

---
+++ typescript-parte03-mais-tecnicas-e-boas-praticas/app/src/decorators/dom-injector.ts
@@ -0,0 +1,16 @@
+export function domInjector(seletor: string) {
+   return function (target: any, propertyKey: string) {
+      console.log(
+         `Modificando prototype ${target.constructor.name} e adicionando gatter para a propriedade ${propertyKey}`
+      );
+      const getter = function () {
+         const elemento = document.querySelector(seletor);
+         console.log(
+            `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`
+         );
+         return elemento;
+      };
+
+      Object.defineProperty(target, propertyKey, { get: getter });
+   };
+}
--- typescript-parte03-mais-tecnicas-e-boas-praticas/app/src/views/view.ts
+++ typescript-parte03-mais-tecnicas-e-boas-praticas/app/src/views/view.ts
@@ -1,12 +1,9 @@
-import { inspect } from "../decorators/inspect.js";
-import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
-
 export abstract class View<T> {
    protected elemento: HTMLElement;
 
    constructor(seletor: string) {
       const elemento = document.querySelector(seletor);
-      
+
       if (elemento) {
          this.elemento = elemento as HTMLElement;
       } else {
@@ -14,8 +11,6 @@ export abstract class View<T> {
       }
    }
 
-   @logarTempoDeExecucao(true)
-   @inspect
    public update(model: T): void {
       let template = this.template(model);
       this.elemento.innerHTML = template;
